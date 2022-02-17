be085bc4f660ef73770ba25e4d1f49a2a8fb5e3d
ansarsufiyana <ansarali106@gmail.com>
2022-02-18 01:19:17 +0500

    its updated

--- pp.js
+++ pp.js
@@ -19,39 +19,29 @@ function validationAll(){
 	if(emailValue == ''){
 		document.getElementById('eror').innerHTML = "**fill the email";
 		return false;
-	}else{
-		document.getElementById('eror').innerHTML = "";
-	
-	};
-	if (emailValue.search(/[@]/) == -1) {
+	}else if (emailValue.search(/[@]/) == -1) {
 		document.getElementById("eror").innerHTML = "**@ is missing ";
 		return false;
 	};
 
   //check empty password field  
+  //minimum password length validation  
+  //maximum length of password validation  
+  // password must contain uppercase letter
+  // passwrod must contain lowercase letter
   if(passValue == "") {  
      document.getElementById("passerror").innerHTML = "**Fill the password please!";  
      return false;  
-    };
-   
- //minimum password length validation  
-  if(passValue.length < 8) {  
+    }else if(passValue.length < 8) {  
      document.getElementById("passerror").innerHTML = "**Password length must be atleast 8 characters";  
      return false;  
-    }; 
-  
-//maximum length of password validation  
-  if(passValue.length > 15) {  
+    }else if(passValue.length > 15) {  
      document.getElementById("passerror").innerHTML = "**Password length must not exceed 15 characters";  
      return false;  
-    };
-// password must contain uppercase letter
-  if (passValue.search(/[A-Z]/) == -1) {
+    }else if (passValue.search(/[A-Z]/) == -1) {
 	document.getElementById("passerror").innerHTML = "**Your password needs at least one upper case letter. ";
 	return false;
-    };
-// passwrod must contain lowercase letter
-  if (passValue.search(/[a-z]/) == -1) {
+    }else if (passValue.search(/[a-z]/) == -1) {
 	document.getElementById("passerror").innerHTML = "**Your password needs at least one lower case letter. ";
 	return false;
   };
@@ -81,45 +71,41 @@ function validationSignup(){
 		document.getElementById('name-error').innerHTML = "**name must not be empty"
 		return false
 	};
+	// checks if the email is empty
 
-// checks if the email is empty
 	if(eamilSignup == ''){
 		document.getElementById('email-signup-error').innerHTML = "**fill the email";
 		return false;
-	};
-	if (eamilSignup.search(/[@]/) == -1) {
+	}else if (eamilSignup.search(/[@]/) == -1) {
 		document.getElementById("email-signup-error").innerHTML = "**@ is missing ";
 		return false;
 	};
 
   //check empty password field  
+  //minimum password length validation  
+  //maximum length of password validation  
+  // password must contain uppercase letter
+  // passwrod must contain lowercase letter
   if(passSignup == "") {  
      document.getElementById("pass-Signup-error").innerHTML = "**Fill the password please!";  
      return false;  
-    };
-   
- //minimum password length validation  
-  if(passSignup.length < 8) {  
+    }else if(passSignup.length < 8) {  
      document.getElementById("pass-Signup-error").innerHTML = "**atleast 8 characters";  
      return false;  
-    }; 
-  
-//maximum length of password validation  
-  if(passSignup.length > 15) {  
+    }else if(passSignup.length > 15) {  
      document.getElementById("pass-Signup-error").innerHTML = "**don't exceed 15 characters";  
      return false;  
-    };
-// password must contain uppercase letter
-  if (passSignup.search(/[A-Z]/) == -1) {
+    }else if (passSignup.search(/[A-Z]/) == -1) {
 	document.getElementById("pass-Signup-error").innerHTML = "**least one uppercase letter. ";
 	return false;
-  }
-// passwrod must contain lowercase letter
-  if (passSignup.search(/[a-z]/) == -1) {
+  }else if (passSignup.search(/[a-z]/) == -1) {
 	document.getElementById("pass-Signup-error").innerHTML = "**one lowercase letter. ";
 	return false;
   };
-  if (password-match !== passSignup){
+
+// confroms   if the password is same or not
+  if (passwordMatch.value !== passSignup.value){
 	document.getElementById("pass-match-error").innerHTML = "**password not matching. ";
   }
 };
+//lets update it on github
